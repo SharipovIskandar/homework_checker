0619af9b12f7e66d7066291990ad7f41b54b4384
Iskandar Sharipov <koklok444@gmail.com>
2025-04-13 08:24:48 -0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,54 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make(1234),
             'role_id' => 1
         ]);
+        \App\Models\User::factory()->create([
+            'username' => 'sakina',
+            'email' => 'sakina@gmail.com',
+            'password' => Hash::make(1234),
+            'role_id' => 1
+        ]);
+        \App\Models\User::factory()->create([
+            'username' => 'abdulqayum',
+            'email' => 'abdulqayum@gmail.com',
+            'password' => Hash::make(1234),
+            'role_id' => 3
+        ]);
+        \App\Models\User::factory()->create([
+            'username' => 'abdulrohman',
+            'email' => 'abdulrohman@gmail.com',
+            'password' => Hash::make(1234),
+            'role_id' => 3
+        ]);
+        \App\Models\User::factory()->create([
+            'username' => 'alijon',
+            'email' => 'alijon@gmail.com',
+            'password' => Hash::make(1234),
+            'role_id' => 3
+        ]);
+        \App\Models\User::factory()->create([
+            'username' => 'mavjuda',
+            'email' => 'mavjuda@gmail.com',
+            'password' => Hash::make(1234),
+            'role_id' => 3
+        ]);
+        \App\Models\User::factory()->create([
+            'username' => 'sevinch',
+            'email' => 'sevinch@gmail.com',
+            'password' => Hash::make(1234),
+            'role_id' => 3
+        ]);
+        \App\Models\User::factory()->create([
+            'username' => 'vazira',
+            'email' => 'vazira@gmail.com',
+            'password' => Hash::make(1234),
+            'role_id' => 3
+        ]);
+        \App\Models\User::factory()->create([
+            'username' => 'umidaxon',
+            'email' => 'umidaxon@gmail.com',
+            'password' => Hash::make(1234),
+            'role_id' => 3
+        ]);
         $this->call(SubjectSeeder::class);
     }
 }
